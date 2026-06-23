3dd0c8dbc9d3017e59eb6fd633dc4dd278c77072
brookolli <brookolli@proton.me>
2026-06-23 15:40:13 -0500

    disable reverse path filtering in networking module

--- modules/system/networking/default.nix
+++ modules/system/networking/default.nix
@@ -3,6 +3,7 @@
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
   networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  networking.firewall.checkReversePath = false;
 
   networking.firewall = {
     allowedTCPPorts = [
