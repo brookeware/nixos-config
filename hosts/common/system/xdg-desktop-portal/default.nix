e31439e3d05944bb3ce25cfe6186ffc8234b4357
brookolli <brookolli@proton.me>
2026-09-11 04:56:16 -0500

    switch xdg portal file chooser to gtk

--- hosts/common/system/xdg-desktop-portal/default.nix
+++ hosts/common/system/xdg-desktop-portal/default.nix
@@ -22,7 +22,7 @@
         default = ["gtk"];
         "org.freedesktop.impl.portal.ScreenCast" = ["wlr"];
         "org.freedesktop.impl.portal.Screenshot" = ["wlr"];
-        "org.freedesktop.impl.portal.FileChooser" = ["termfilechooser"];
+        "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
       };
     };
   };
