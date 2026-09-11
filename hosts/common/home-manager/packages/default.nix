2beaab28148f015f4f6d442bb2306791ee04bab6
brookolli <brookolli@proton.me>
2026-09-11 10:43:48 -0500

    sort home manager packages

--- hosts/common/home-manager/packages/default.nix
+++ hosts/common/home-manager/packages/default.nix
@@ -1,28 +1,28 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    jq
     btop
-    vesktop
-    feishin
+    cmake
     fastfetch
+    feishin
+    flameshot
     hyfetch
-    prismlauncher
-    wl-clipboard
-    kdePackages.kate
-    mpv
-    vlc
+    jq
     kdePackages.ark
     kdePackages.gwenview
+    kdePackages.kate
     kdePackages.kio
-    kdePackages.kio-fuse
-    kdePackages.kio-extras
     kdePackages.kio-admin
-    obs-studio
-    cmake
-    lxqt.pavucontrol-qt
-    flameshot
+    kdePackages.kio-extras
+    kdePackages.kio-fuse
     krita
     libreoffice-qt6-fresh
+    lxqt.pavucontrol-qt
+    mpv
+    obs-studio
+    prismlauncher
+    vesktop
+    vlc
+    wl-clipboard
   ];
 }
